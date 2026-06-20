4b1a1416f37df34e496cd7306be787544f8d2d37
abdulkhan <khanabdulrahim378@gmail.com>
2026-06-20 17:55:44 +0530

    dbsnetadded

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -26,3 +26,10 @@ resource "azurerm_subnet" "appsnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.0.1.0/26"]
 }
+
+resource "azurerm_subnet" "dbrefsnet" {
+  name                 = "db-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.2.0/26"]
+}
